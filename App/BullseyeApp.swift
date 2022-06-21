d1cedac2951223f023e9e6ac4a4ae702f2fc84fa
Dakshay <dakshayahuja02@gmail.com>
2022-06-21 11:20:33 +0530

    First commit

---
+++ App/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Shared
+//
+//  Created by Dakshay Ahuja on 16/06/22.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
